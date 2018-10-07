f4d1a1bf5671beb9a8297fdff2b19e4e68b4c9e9
rico <ricoyangjing@didichuxing.com>
2018-10-07 18:22:01 +0800

    pass smock test

--- Example/RCCache/RCViewController.m
+++ Example/RCCache/RCViewController.m
@@ -11,11 +11,15 @@
 #import "RCMemoryCache.h"
 #import "RCDiskCache.h"
 #import "YYMemoryCache.h"
+#import "DCCache.h"
+#import "RCCache.h"
 
 @interface RCViewController ()
 
 @property (nonatomic, strong) RCMemoryCache *memCache;
 @property (nonatomic, strong) RCDiskCache *diskCache;
+@property (nonatomic, strong) RCCache *rccache;
+@property (nonatomic, strong) DCCache *dccache;
 
 @end
 
@@ -42,6 +46,7 @@
 //    b = [memCache containsObjectForKey:key2];
     
     //test for RCDiskCache
+    /*
     NSString *cacheFolder = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) firstObject];
     NSString *path = [cacheFolder stringByAppendingPathComponent:@"RCCache-demo"];
     self.diskCache = [[RCDiskCache alloc] initWithPath:path];
@@ -51,6 +56,31 @@
     BOOL b = [self.diskCache containsObjectForKey:key];
     [self.diskCache removeObjectForKey:key];
     [self.diskCache containsObjectForKey:key withBlock:^(NSString * _Nonnull key, BOOL contains) {
+        if (contains) {
+            NSLog(@"%@", key);
+        }
+    }];
+    */
+    
+    //test for RCCache
+    /*
+    self.rccache = [[RCCache alloc] initWithName:@"demo"];
+    NSString *key = @"rccache-demo-key";
+    BOOL b = [self.rccache containsObjectForKey:key];
+    [self.rccache setObject:@"rccache-demo-value" forKey:key];
+    [self.rccache containsObjectForKey:key withBlock:^(NSString * _Nonnull key, BOOL contains) {
+        if (contains) {
+            NSLog(@"%@", key);
+        }
+    }];
+    */
+    
+    //test for RCCache
+    self.dccache = [[DCCache alloc] initWithName:@"demo"];
+    NSString *key = @"dccache-demo-key";
+    BOOL b = [self.dccache containsObjectForKey:key];
+    [self.dccache setObject:@"rccache-demo-value" forKey:key];
+    [self.dccache containsObjectForKey:key withBlock:^(NSString * _Nonnull key, BOOL contains) {
         if (contains) {
             NSLog(@"%@", key);
         }
