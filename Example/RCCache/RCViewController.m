5c796dda566d1cb6283729fb8763d595ee46c8f6
rico <ricoyangjing@didichuxing.com>
2018-10-07 18:13:38 +0800

    add simple RCDiskCache test

--- Example/RCCache/RCViewController.m
+++ Example/RCCache/RCViewController.m
@@ -9,11 +9,13 @@
 #import "RCViewController.h"
 //#import <RCCache/RCMemoryCache.h>
 #import "RCMemoryCache.h"
-//#import "YYMemoryCache.h"
+#import "RCDiskCache.h"
+#import "YYMemoryCache.h"
 
 @interface RCViewController ()
 
 @property (nonatomic, strong) RCMemoryCache *memCache;
+@property (nonatomic, strong) RCDiskCache *diskCache;
 
 @end
 
@@ -24,20 +26,35 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 //    YYMemoryCache *c
-    self.memCache = [[RCMemoryCache alloc] init];
-    RCMemoryCache *memCache = self.memCache;
-    memCache.countLimit = 1;
-    memCache.costLimit = 10;
-//    memCache.ageLimit = 6;
-    NSString *key = @"firstObjectKey";
-    NSString *key2 = @"secondObjectKey";
-    [memCache setObject:@"firstObject" forKey:key];
-    [memCache setObject:@"secondObject" forKey:key2];
-    BOOL b = [memCache containsObjectForKey:key];
-    id obj = [memCache objectForKey:key];
-    obj = [memCache objectForKey:key2];
-//    [memCache removeObjectForKey:key];
-    b = [memCache containsObjectForKey:key2];
+//    self.memCache = [[RCMemoryCache alloc] init];
+//    RCMemoryCache *memCache = self.memCache;
+//    memCache.countLimit = 1;
+//    memCache.costLimit = 10;
+////    memCache.ageLimit = 6;
+//    NSString *key = @"firstObjectKey";
+//    NSString *key2 = @"secondObjectKey";
+//    [memCache setObject:@"firstObject" forKey:key];
+//    [memCache setObject:@"secondObject" forKey:key2];
+//    BOOL b = [memCache containsObjectForKey:key];
+//    id obj = [memCache objectForKey:key];
+//    obj = [memCache objectForKey:key2];
+////    [memCache removeObjectForKey:key];
+//    b = [memCache containsObjectForKey:key2];
+    
+    //test for RCDiskCache
+    NSString *cacheFolder = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) firstObject];
+    NSString *path = [cacheFolder stringByAppendingPathComponent:@"RCCache-demo"];
+    self.diskCache = [[RCDiskCache alloc] initWithPath:path];
+    NSString *key = @"key1";
+    [self.diskCache setObject:@"value1" forKey:key];
+    
+    BOOL b = [self.diskCache containsObjectForKey:key];
+    [self.diskCache removeObjectForKey:key];
+    [self.diskCache containsObjectForKey:key withBlock:^(NSString * _Nonnull key, BOOL contains) {
+        if (contains) {
+            NSLog(@"%@", key);
+        }
+    }];
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
